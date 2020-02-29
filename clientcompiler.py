add7ed75730ca160e2dbee41517a1cc8b04a9dad
Rahul Devadiga <43218543+RahulDevadiga@users.noreply.github.com>
2020-02-29 14:28:25 +0530

    MultiThreading, multiple times compile feature added

--- clientcompiler.py
+++ clientcompiler.py
@@ -1,28 +1,38 @@
 from flask import Flask,redirect, url_for, request, render_template
 import subprocess
 import socket
+import sys
 
 app = Flask(__name__)
-client = None
+soc = None
 langId = {"c":0,"cpp":1,"java":2,"python2":3,"python3":4}
 
 @app.route('/')
 def home():
-	global client
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.connect(('127.0.0.1', 8080))
+	global soc
+	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host = "127.0.0.1"
+	port = 8000
+	try:
+		soc.connect((host, port))
+	except:
+		print("Connection Error")
+		sys.exit()
 	return render_template('compilerclient.html')
 	#return "hello"
 
 @app.route('/compile', methods = ['GET', 'POST'])
 def compile():
-	global client
+	global soc
 	code = request.form.get('code')
 	lang = request.form.get('language')
 	print(code,lang)
-	client.send(b"".join([code.encode(), str(langId[lang]).encode()]))
-	compiledOutput = client.recv(4096)
-	client.close()
-	return render_template('compilerclient.html',outputreturned = compiledOutput.decode(), codereturned = code, langreturned = lang)
+	print("Socket os",soc)
+	
+	soc.sendall(b"".join([code.encode(), str(langId[lang]).encode()]))
+	outputreturned = soc.recv(5120).decode("utf8")
+	
+	print("###################output returned ",outputreturned)
+	return render_template('compilerclient.html',outputreturned = outputreturned, codereturned = code, langreturned = lang)
 	
 app.run(host = '127.0.0.1', port = 5000)
--- servercompiler.py
+++ servercompiler.py
@@ -1,10 +1,10 @@
 import subprocess
 import socket
+import sys
+import traceback
+from threading import Thread
 
-langName = {0:"c",1:"cpp",2:"java",3:"python2",4:"python3"}
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('0.0.0.0', 8080))
-serv.listen(5)
+
 
 
 def getOutput(code, lang):
@@ -44,21 +44,58 @@ def getOutput(code, lang):
 		output = subprocess.run(["./out1"], stdout = subprocess.PIPE)
 	return output.stdout
 
-while True:
-    conn, addr = serv.accept()
-    from_client = b''
-    while True:
-        data = conn.recv(4096)
-        from_client = b''.join([from_client,data])
-        from_client = from_client.decode()
-        
-        print(from_client)
-        lang = langName[int(from_client[-1])]
-        code = from_client[:len(from_client)-1:]
-        output = getOutput(code, lang)
-        print("output ",output)
-        conn.send(output)
-    conn.close()
-    print('client disconnected')
+def start_server():
+   host = "127.0.0.1"
+   port = 8000 # arbitrary non-privileged port
+   soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+   print("Socket created")
+   try:
+      soc.bind((host, port))
+   except:
+      print("Bind failed. Error : " + str(sys.exc_info()))
+      sys.exit()
+   soc.listen(6) # queue up to 6 requests
+   print("Socket now listening")
+   # infinite loop- do not reset for every requests
+   while True:
+      connection, address = soc.accept()
+      ip, port = str(address[0]), str(address[1])
+      print("Connected with " + ip + ":" + port)
+      try:
+         Thread(target=clientThread, args=(connection, ip, port)).start()
+      except:
+         print("Thread did not start.")
+         traceback.print_exc()
+   soc.close()
 
-
+def clientThread(connection, ip, port, max_buffer_size = 5120):
+   is_active = True
+   while is_active:
+      client_input = receive_input(connection, max_buffer_size)
+      if b"--QUIT--" in client_input:
+         print("Client is requesting to quit")
+         connection.close()
+         print("Connection " + ip + ":" + port + " closed")
+         is_active = False
+      else:
+         print("Processed result: {}".format(client_input))
+         connection.sendall(client_input)
+         
+		 
+def receive_input(connection, max_buffer_size):
+   print("recieve input called")
+   client_input = connection.recv(max_buffer_size)
+   client_input_size = sys.getsizeof(client_input)
+   if client_input_size > max_buffer_size:
+      print("The input size is greater than expected {}".format(client_input_size))
+   from_client = client_input.decode("utf8").rstrip()
+   lang = langName[int(from_client[-1])]
+   code = from_client[:len(from_client)-1:]
+   output = getOutput(code, lang)
+   print("output ",output)
+   return output
+  
+   
+langName = {0:"c",1:"cpp",2:"java",3:"python2",4:"python3"}
+start_server()
